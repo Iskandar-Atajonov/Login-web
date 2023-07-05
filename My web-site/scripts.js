1863336b8284ec0d52eb306a56a469630994b503
Iskandar <122857400+Iskandar-Atajonov@users.noreply.github.com>
2023-07-05 16:12:05 +0500

    Add files via upload

---
+++ My web-site/scripts.js
@@ -0,0 +1,17 @@
+document.getElementById("btn").onclick = () =>{
+    const person = {
+name : document.getElementById("name").value,
+phone_number : document.getElementById("surname").value,
+email : document.getElementById("email").value,
+date_birth : document.getElementById("password").value
+}
+console.log(person)
+let name = person.name
+// console.log(name.substr(0, 9))
+// console.log(name.indexof("I"))
+}
+document.getElementById('surname').addEventListener('input',()=>{
+if(document.getElementById("surname").length = 13){
+
+}
+})
